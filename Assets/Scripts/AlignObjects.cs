feb0ab875e865edd3b1728a9e70128d396efdb41
jswz72 <jacobdasword@gmail.com>
2019-04-02 11:04:38 -0400

    init multi-point, rotation with objs child of align point

--- Assets/Scripts/AlignObjects.cs
+++ Assets/Scripts/AlignObjects.cs
@@ -10,29 +10,52 @@ public class AlignObjects : MonoBehaviour
     public GameObject point2;
     public Text statusText;
 
-    uint pointsCaptured = 1;
-    Vector3 translationVectorAvg;
+    uint pointsCaptured = 0;
+    Vector3 translationVectorSum = new Vector3(0, 0, 0);
+    float translationAngleSum = 0f;
     bool acquire = false;
     bool dragging = false;
     Transform toDrag;
     float dist;
     Vector3 offset;
+    GameObject mainCalibrationObj;
     GameObject[] cadObjs;
-    GameObject mainCalibraitonObj;
+    readonly int numCalPoints = 2;
 
     public void CapturePoint()
     {
         if (pointsCaptured == 2)
             return;
+        StartCoroutine("SetAcquire");
+    }
+
+    private IEnumerator SetAcquire()
+    {
+        yield return new WaitForSeconds(0.5f);
         acquire = true;
+
     }
 
     private void Start()
     {
-        //cadObjs = GameObject.FindGameObjectsWithTag("cadobject");
-        mainCalibraitonObj = GameObject.FindGameObjectWithTag("calibration");
+        mainCalibrationObj = GameObject.FindGameObjectWithTag("calibration");
+        cadObjs = GameObject.FindGameObjectsWithTag("cadobject");
 
     }
+
+    private void AcquirePoint()
+    {
+        Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        var calPoint = pointsCaptured == 0 ? point1 : point2;
+        var trans = point - calPoint.transform.position;
+        float yAngleVal = Camera.main.transform.rotation.eulerAngles.y - 180;
+        translationVectorSum += trans;
+        translationAngleSum += yAngleVal;
+        acquire = false;
+        pointsCaptured++;
+        statusText.text = "";
+    }
+
 
     private void Update()
     {
@@ -40,94 +63,70 @@ public class AlignObjects : MonoBehaviour
         {
             statusText.text = "Tap 1st calibration point";
             if (Input.touchCount > 0)
-            {
-                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                var hit = new RaycastHit();
-                if (Physics.Raycast(ray, out hit))
-                {
-                    var trans = hit.point - point1.transform.position;
-                    translationVectorAvg = trans;
-                    acquire = false;
-                    pointsCaptured++;
-                    statusText.text = "";
-                }
-            }
+                AcquirePoint();
         }
         else if (pointsCaptured == 1 && acquire)
         {
             statusText.text = "Tap 2nd calibration point";
             if (Input.touchCount > 0)
             {
-                //var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
-                float yAngleVal = Camera.main.transform.rotation.eulerAngles.y - 180;
-                //var hit = new RaycastHit();
-                //if (Physics.Raycast(ray, out hit))
-                //{
-                //var trans = hit.point - point2.transform.position;
-                var trans = point - point2.transform.position;
-                translationVectorAvg = trans;
-                //translationVectorAvg = (translationVectorAvg + trans) / 2;
-                /*foreach (GameObject go in cadObjs)
-                {
-                    go.transform.position += translationVectorAvg;
-                }*/
-                mainCalibraitonObj.transform.position += translationVectorAvg;
-                mainCalibraitonObj.transform.eulerAngles = new Vector3(0f, yAngleVal, 0f);
-                acquire = false;
-                pointsCaptured++;
-                statusText.text = "";
-                //}
+                AcquirePoint();
+                mainCalibrationObj.transform.position += translationVectorSum / numCalPoints;
+                mainCalibrationObj.transform.eulerAngles = new Vector3(0f, translationAngleSum / numCalPoints, 0f);
             }
         }
         else
+            HandleDrag();
+    }
+
+
+    private void HandleDrag()
+    {
+        Vector3 v3;
+
+        if (Input.touchCount != 1)
         {
-            Vector3 v3;
+            dragging = false;
+            return;
+        }
 
-            if (Input.touchCount != 1)
-            {
-                dragging = false;
-                return;
-            }
+        Touch touch = Input.touches[0];
+        Vector3 pos = touch.position;
 
-            Touch touch = Input.touches[0];
-            Vector3 pos = touch.position;
-
-            if (touch.phase == TouchPhase.Began)
+        if (touch.phase == TouchPhase.Began)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(pos);
+            if (Physics.Raycast(ray, out hit) && (hit.collider.tag == "cadobject"))
             {
-                RaycastHit hit;
-                Ray ray = Camera.main.ScreenPointToRay(pos);
-                if (Physics.Raycast(ray, out hit) && (hit.collider.tag == "cadobject"))
-                {
-                    toDrag = hit.transform;
-                    dist = hit.transform.position.z - Camera.main.transform.position.z;
-                    v3 = new Vector3(pos.x, pos.y, dist);
-                    v3 = Camera.main.ScreenToWorldPoint(v3);
-                    offset = toDrag.position - v3;
-                    dragging = true;
-                }
-            }
-            if (dragging && touch.phase == TouchPhase.Moved)
-            {
-                v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+                toDrag = hit.transform;
+                dist = hit.transform.position.z - Camera.main.transform.position.z;
+                v3 = new Vector3(pos.x, pos.y, dist);
                 v3 = Camera.main.ScreenToWorldPoint(v3);
-                var lastPosition = toDrag.position;
-                toDrag.position = v3 + offset;
-                var newPosition = toDrag.position;
-                var diff = newPosition - lastPosition;
-                foreach (GameObject go in cadObjs)
-                {
-                    if (go != toDrag)
-                    {
-                        go.transform.position += diff;
-                    }
-                }
-
+                offset = toDrag.position - v3;
+                dragging = true;
             }
-            if (dragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled))
+        }
+        if (dragging && touch.phase == TouchPhase.Moved)
+        {
+            v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+            v3 = Camera.main.ScreenToWorldPoint(v3);
+            var lastPosition = toDrag.position;
+            toDrag.position = v3 + offset;
+            var newPosition = toDrag.position;
+            var diff = newPosition - lastPosition;
+            foreach (GameObject go in cadObjs)
             {
-                dragging = false;
+                if (go != toDrag)
+                {
+                    go.transform.position += diff;
+                }
             }
+
+        }
+        if (dragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled))
+        {
+            dragging = false;
         }
     }
 }
