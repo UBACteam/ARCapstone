b9756948999fd57cf8454a85dec96e80cc73ef86
jswz72 <jacobdasword@gmail.com>
2019-04-11 14:38:09 -0400

    switch back to constructing debug obs on the fly, add info on graduation measurements in cm, add actual scene

--- Assets/Scripts/DebugObjects.cs
+++ Assets/Scripts/DebugObjects.cs
@@ -5,22 +5,23 @@ using UnityEngine.UI;
 
 public class DebugObjects : MonoBehaviour
 {
-    public Material debugMaterial;
+    // The material to apply to debug objects
+    public Material debugMaterial;  
+    // The dropdown where the user chooses to what side of the face they want the debug object
     public Dropdown directionDropdown;
+    // The number of graduations on the debug skin - to be used in calculations
+    public int graduations;
+    // To output the size of each graduation
+    public Text measurementText;
 
     bool isDebugging = false;
     GameObject debugObj;
+    GameObject parentObj;
     bool getDebugPoint = false;
 
     void Start()
     {
-        GameObject[] debugObjs = GameObject.FindGameObjectsWithTag("cadobject");
-        foreach (var go in debugObjs)
-        {
-            var dp = go.GetComponent<DebugPlacement>();
-            if (dp)
-                dp.InstantiateDebugObjs();
-        }
+        parentObj = GameObject.FindGameObjectWithTag("calibration");
     }
 
 
@@ -29,18 +30,22 @@ public class DebugObjects : MonoBehaviour
     {
         if (isDebugging)
         {
-            debugObj.GetComponent<Renderer>().enabled = false;
-            //debugObj.SetActive(false);
+            Destroy(debugObj);
             isDebugging = false;
+            measurementText.text = "";
         }
         else
+        {
+            measurementText.text = "Tap face to debug";
             getDebugPoint = true;
+        }
+
     }
 
 
     private void Update()
     {
-        if (!getDebugPoint || !Input.GetMouseButtonDown(0))
+        if (!getDebugPoint || Input.touchCount <= 0)
             return;
 
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -52,35 +57,46 @@ public class DebugObjects : MonoBehaviour
             if (!dp || !dp.debug || go.tag != "cadobject")
                 return;
 
-            // Vector facing into plane that was hit
-            Vector3 facing = -hit.normal;
+            // Vector facing out from plane that was hit
+            Vector3 normal = hit.normal;
+            float xMag = dp.bounds.size.x;
+            float yMag = dp.bounds.size.y;
+            Vector3 right = go.transform.right;
 
-            GameObject[] directions = new GameObject[4];
-            directions[2] = dp.debugObjs["up"];
-            directions[3] = dp.debugObjs["down"];
-            if (facing == go.transform.forward)
+            // Reassign x-bounds depending on side chosen
+            if (normal == go.transform.right || normal == -go.transform.right)
             {
-                directions[0] = dp.debugObjs["left"];
-                directions[1] = dp.debugObjs["right"];
+                xMag = dp.bounds.size.z;
             }
-            else if (facing == -go.transform.forward)
-            {
-                directions[0] = dp.debugObjs["right"];
-                directions[1] = dp.debugObjs["left"];
-            }
-            else if (facing == -go.transform.right)
-            {
-                directions[0] = dp.debugObjs["backward"];
-                directions[1] = dp.debugObjs["forward"];
-            }
-            else if (facing == go.transform.right)
-            {
-                directions[0] = dp.debugObjs["forward"];
-                directions[1] = dp.debugObjs["backward"];
-            }
-            debugObj = directions[directionDropdown.value];
-            debugObj.SetActive(true);
-            debugObj.GetComponent<Renderer>().enabled = true;
+            // Account top/bottom placmenet when choosing sides of object
+            if (normal == go.transform.right)
+                right = go.transform.forward;
+            else if (normal == -go.transform.right)
+                right = -go.transform.forward;
+
+            // Account for top/bottom placement when choosing back of object
+            int rightMult = (normal == go.transform.forward) ? -1 : 1;
+
+            // Directions to place debug object, depending on dropdown values
+            Vector3[] directions = new Vector3[] {
+                (Quaternion.AngleAxis(90, Vector3.up) * normal) * xMag,
+                (Quaternion.AngleAxis(-90, Vector3.up) * normal) * xMag,
+                (Quaternion.AngleAxis(90, right * rightMult) * normal) * yMag,
+                (Quaternion.AngleAxis(-90, right * rightMult) * normal) * yMag,
+            };
+            Vector3 direction = directions[directionDropdown.value];
+            // Place new debug object beside current object.
+            // Add same parent so will move with the rest of the scene
+            GameObject newObj = Instantiate(
+                go,
+                go.transform.position + direction,
+                go.transform.rotation,
+                parentObj.transform
+                );
+            measurementText.text = ((xMag * 100) / graduations).ToString("F2") + "cm/grad horizontal\n" + 
+                ((yMag * 100) / graduations).ToString("F2") + "cm/grad vertical";
+            newObj.GetComponent<Renderer>().material = debugMaterial;
+            debugObj = newObj;
             isDebugging = true;
             getDebugPoint = false;
         }
--- Assets/Scripts/DebugPlacement.cs
+++ Assets/Scripts/DebugPlacement.cs
@@ -5,38 +5,14 @@ using UnityEngine;
 public class DebugPlacement : MonoBehaviour
 {
     public bool debug = false;
-    public Material debugMaterial;
+    public Bounds bounds;
 
-    public Dictionary<string, GameObject> debugObjs =
-        new Dictionary<string, GameObject>();
-
-    Quaternion rotation;
-    Transform parent;
-
-    private void AddDebugObj(string direction, Vector3 position)
-    {
-        var debugObj = Instantiate(gameObject, position, rotation, parent);
-        var rend = debugObj.GetComponent<Renderer>();
-        rend.enabled = false;
-        debugObj.SetActive(false);
-        rend.material = debugMaterial;
-        debugObjs.Add(direction, debugObj);
-    }
-
-    public void InstantiateDebugObjs()
+    private void Awake()
     {
         if (!debug)
             return;
-        rotation = gameObject.transform.rotation;
-        parent = gameObject.transform.parent;
-        Vector3 bounds = gameObject.GetComponent<Renderer>().bounds.size;
-        Vector3 position = gameObject.transform.position;
-
-        AddDebugObj("left", position + (-Vector3.right * bounds.x));
-        AddDebugObj("right", position + (Vector3.right * bounds.x));
-        AddDebugObj("up", position + (Vector3.up * bounds.y));
-        AddDebugObj("down", position + (-Vector3.up * bounds.y));
-        AddDebugObj("forward", position + (Vector3.forward * bounds.z));
-        AddDebugObj("backward", position + (-Vector3.forward * bounds.z));
+        // Initial bounds. 
+        // Inititialized here because bound directions after rotation will not be true to visible object size
+        bounds = gameObject.GetComponent<Renderer>().bounds;
     }
 }
