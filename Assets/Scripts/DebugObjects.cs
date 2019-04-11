e72459ac0c1d20f9e6dece7edfc771a147b98baa
jswz72 <jacobdasword@gmail.com>
2019-04-11 09:59:59 -0400

    attempt adding debug object on the fly to any face

--- Assets/Scripts/DebugObjects.cs
+++ Assets/Scripts/DebugObjects.cs
@@ -1,59 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DebugObjects : MonoBehaviour
 {
     public Material debugMaterial;
+    public Dropdown directionDropdown;
 
-    GameObject[] cadObjs;
-    List<GameObject> debugObjs = new List<GameObject>();
+    bool isDebugging = false;
+    GameObject debugObj;
     GameObject parentObj;
-    bool instantiated = false;
+    bool getDebugPoint = false;
 
-    // Use this for initialization
     void Start()
     {
-        cadObjs = GameObject.FindGameObjectsWithTag("cadobject");
         parentObj = GameObject.FindGameObjectWithTag("calibration");
     }
 
-    void InitDebugObjects()
-    {
-        foreach (var go in cadObjs)
-        {
-            DebugPlacement dp = go.GetComponent<DebugPlacement>();
-            if (dp && dp.debug)
-            {
-                // Place new debug object beside current object.
-                // Add same parent so will move with the rest of the scene
-                GameObject newObj = Instantiate(
-                    go,
-                    go.transform.position + (-go.transform.right * dp.bounds.size.x),
-                    go.transform.rotation,
-                    parentObj.transform
-                    );
-                newObj.GetComponent<Renderer>().material = debugMaterial;
-                debugObjs.Add(newObj);
-            }
-        }
-        instantiated = true;
-    }
 
-    void ToggleDebugObjects()
-    {
-        foreach (var go in debugObjs)
-        {
-            go.GetComponent<Renderer>().enabled = !go.GetComponent<Renderer>().enabled;
-        }
-    }
 
-    void ShowDebug()
+    public void DebugObject()
     {
-        if (!instantiated)
-            InitDebugObjects();
+        if (isDebugging)
+        {
+            Destroy(debugObj);
+            isDebugging = false;
+        }
         else
-            ToggleDebugObjects();
+        {
+            getDebugPoint = true;
+        }
+
     }
 
+
+    private void Update()
+    {
+        if (!getDebugPoint || Input.touchCount <= 0)
+            return;
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var hit = new RaycastHit();
+        if (Physics.Raycast(ray, out hit))
+        {
+            var go = hit.transform.gameObject;
+            DebugPlacement dp = go.GetComponent<DebugPlacement>();
+            if (!dp || !dp.debug || go.tag != "cadobject")
+                return;
+
+            // Vector facing out from plane that was hit
+            Vector3 normal = hit.normal;
+            float xMag = dp.bounds.size.x;
+            float yMag = dp.bounds.size.y;
+            Vector3 right = go.transform.right;
+
+            // Reassign x-bounds depending on side chosen
+            if (normal == go.transform.right || normal == -go.transform.right)
+            {
+                xMag = dp.bounds.size.z;
+            }
+            // Account top/bottom placmenet when choosing sides of object
+            if (normal == go.transform.right)
+                right = go.transform.forward;
+            else if (normal == -go.transform.right)
+                right = -go.transform.forward;
+
+            // Account for top/bottom placement when choosing back of object
+            int rightMult = (normal == go.transform.forward) ? -1 : 1;
+
+            // Directions to place debug object, depending on dropdown values
+            Vector3[] directions = new Vector3[] {
+                (Quaternion.AngleAxis(90, Vector3.up) * normal) * xMag,
+                (Quaternion.AngleAxis(-90, Vector3.up) * normal) * xMag,
+                (Quaternion.AngleAxis(90, right * rightMult) * normal) * yMag,
+                (Quaternion.AngleAxis(-90, right * rightMult) * normal) * yMag,
+            };
+            Vector3 direction = directions[directionDropdown.value];
+            // Place new debug object beside current object.
+            // Add same parent so will move with the rest of the scene
+            GameObject newObj = Instantiate(
+                go,
+                go.transform.position + direction,
+                go.transform.rotation,
+                parentObj.transform
+                );
+            newObj.GetComponent<Renderer>().material = debugMaterial;
+            debugObj = newObj;
+            isDebugging = true;
+            getDebugPoint = false;
+        }
+    }
 }
